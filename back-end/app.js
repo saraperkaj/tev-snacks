00f15f984f62ac3e17744e7cf9a524e95eadc381
Sara Perkaj <saraperkaj@pursuit.org>
2022-02-14 10:50:25 -0500

    app.js configuration

--- back-end/app.js
+++ back-end/app.js
@@ -4,10 +4,14 @@ const express = require("express");
 
 // CONFIGURATION
 const app = express();
+const cors = require("cors");
 
 // MIDDLEWARE
-
+app.use(cors());
+app.use(express.json());
 // ROUTES
-
+app.get("/", (request, response) => {
+  response.send("Get Snack'n at Snack-a-log!");
+});
 // EXPORT
 module.exports = app;
